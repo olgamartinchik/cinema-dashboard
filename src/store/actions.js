305669b2821a7425c80446c7394acb262f128673
olgamartinchik <sintcerova@mail.ru>
2025-02-16 18:00:03 +0300

    fix: actions

--- src/store/actions.js
+++ src/store/actions.js
@@ -15,20 +15,6 @@ export const deleteCategory = (id, name) => ({
   payload: { id, name },
 });
 
-export const addSubCategory = (categoryId, subCategory) => ({
-  type: ActionTypes.ADD_SUBCATEGORY,
-  payload: { categoryId, subCategory },
-});
-
-export const updateSubCategory = (categoryId, categoryName, subCategory) => ({
-  type: ActionTypes.UPDATE_SUBCATEGORY,
-  payload: { categoryId, categoryName, subCategory },
-});
-
-// export const deleteSubCategory = (categoryId, subCategoryId) => ({
-//   type: ActionTypes.DELETE_SUBCATEGORY,
-//   payload: { categoryId, subCategoryId },
-// });
 export const deleteSubCategory = (category, subCategory) => ({
   type: ActionTypes.DELETE_SUBCATEGORY,
   payload: { category, subCategory },
